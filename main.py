985398b880b74cdff9f74f6c59dffc98d260cb70
梁辰有点菜 <36767143+c1318821837@users.noreply.github.com>
2022-03-26 15:38:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ model=FastHan()
 
 @app.route('/', methods = ['GET', 'POST'])  # 主页地址,“装饰器”
 def content_words():
+    #获取html方法为post的表单内容
     if request.method=="POST":
+        #request.values.get("")获取html页面name为xx的内容
         item_content=request.values.get('item_text')
         answer=model(item_content,target="CWS")
     return render_template('index.html',answer=answer[0],item_content=item_content)  # 把index.html文件读进来，再交给浏览器
+#额外声明xx=xx
 
 
 if __name__ == '__main__':
